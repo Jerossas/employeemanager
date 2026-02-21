123218f2513c59aa763d64e52f5a1b348baa3a1d
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-02-21 15:09:39 -0500

    feature(domain): add Employee domain model

---
+++ src/main/java/com/dunnwr/employeemanager/domain/model/Employee.java
@@ -0,0 +1,15 @@
+package com.dunnwr.employeemanager.domain.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phoneNumber;
+    private String imageUrl;
+    private String employeeCode;
+
+}
