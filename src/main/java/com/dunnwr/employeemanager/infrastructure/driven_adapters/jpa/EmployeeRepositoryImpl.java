ee84516a106d5e8630e03b9a04aa55da645c82e7
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-02-21 22:48:30 -0500

    feat(jpa-adapter): add implementation for save method defined in EmployeeRepository interface

--- src/main/java/com/dunnwr/employeemanager/infrastructure/driven_adapters/jpa/EmployeeRepositoryImpl.java
+++ src/main/java/com/dunnwr/employeemanager/infrastructure/driven_adapters/jpa/EmployeeRepositoryImpl.java
@@ -1,14 +1,28 @@
 package com.dunnwr.employeemanager.infrastructure.driven_adapters.jpa;
 
+import com.dunnwr.employeemanager.domain.model.Employee;
 import com.dunnwr.employeemanager.domain.model.gateways.EmployeeRepository;
+import com.dunnwr.employeemanager.infrastructure.driven_adapters.jpa.entities.EmployeeEntity;
+import com.dunnwr.employeemanager.infrastructure.helpers.mapper.EmployeeMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class EmployeeRepositoryImpl implements EmployeeRepository {
 
     private final EmployeeJpaRepository jpaRepository;
+    private final EmployeeMapper employeeMapper;
 
-    public EmployeeRepositoryImpl(EmployeeJpaRepository jpaRepository){
+    public EmployeeRepositoryImpl(EmployeeJpaRepository jpaRepository, EmployeeMapper employeeMapper){
         this.jpaRepository = jpaRepository;
+        this.employeeMapper = employeeMapper;
+    }
+
+    @Override
+    public Employee save(Employee employee){
+
+        EmployeeEntity entity = employeeMapper.toEntity(employee);
+        EmployeeEntity savedEntity = jpaRepository.save(entity);
+
+        return employeeMapper.toDomain(savedEntity);
     }
 }
