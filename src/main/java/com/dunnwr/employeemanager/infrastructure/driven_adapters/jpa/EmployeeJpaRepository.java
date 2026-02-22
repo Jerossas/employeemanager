075c99061b4b6cf2855829c66022e5ff77fec610
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-02-21 22:39:45 -0500

    fix(jpa-adapter): change generic signature from Employee to EmployeeEntity

--- src/main/java/com/dunnwr/employeemanager/infrastructure/driven_adapters/jpa/EmployeeJpaRepository.java
+++ src/main/java/com/dunnwr/employeemanager/infrastructure/driven_adapters/jpa/EmployeeJpaRepository.java
@@ -1,6 +1,6 @@
 package com.dunnwr.employeemanager.infrastructure.driven_adapters.jpa;
 
-import com.dunnwr.employeemanager.domain.model.Employee;
+import com.dunnwr.employeemanager.infrastructure.driven_adapters.jpa.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeJpaRepository extends JpaRepository<Employee, Long> {}
+public interface EmployeeJpaRepository extends JpaRepository<EmployeeEntity, Long> {}
