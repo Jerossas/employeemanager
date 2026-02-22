cb4a5218f8bc686d46ea6c50bfef2ec4764ea467
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-02-21 22:44:14 -0500

    feat(domain): add abstract method save to the EmployeeRepository

--- src/main/java/com/dunnwr/employeemanager/domain/model/gateways/EmployeeRepository.java
+++ src/main/java/com/dunnwr/employeemanager/domain/model/gateways/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package com.dunnwr.employeemanager.domain.model.gateways;
 
+import com.dunnwr.employeemanager.domain.model.Employee;
+
 public interface EmployeeRepository {
+
+    Employee save(Employee employee);
 }
