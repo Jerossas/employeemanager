577b30473f789901f065de1df80e7ca4bf2487e4
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-02-22 19:24:59 -0500

    feat(domain): add abstract method findAll to the EmployeeRepository

--- src/main/java/com/dunnwr/employeemanager/domain/model/gateways/EmployeeRepository.java
+++ src/main/java/com/dunnwr/employeemanager/domain/model/gateways/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.dunnwr.employeemanager.domain.model.gateways;
 
 import com.dunnwr.employeemanager.domain.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository {
 
     Employee save(Employee employee);
+    List<Employee> findAll();
 }
