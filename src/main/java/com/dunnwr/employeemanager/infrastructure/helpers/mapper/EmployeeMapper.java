4392fbe3dcaaf7ced3d6e5a43ccecc16ffa038a4
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-02-22 19:24:15 -0500

    feat(infrastructure): add functionality to EmployeeMapper to convert a list of entities to a list of domain

--- src/main/java/com/dunnwr/employeemanager/infrastructure/helpers/mapper/EmployeeMapper.java
+++ src/main/java/com/dunnwr/employeemanager/infrastructure/helpers/mapper/EmployeeMapper.java
@@ -4,9 +4,12 @@ import com.dunnwr.employeemanager.domain.model.Employee;
 import com.dunnwr.employeemanager.infrastructure.driven_adapters.jpa.entities.EmployeeEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
 
     EmployeeEntity toEntity(Employee employee);
     Employee toDomain(EmployeeEntity employee);
+    List<Employee> toDomainList(List<EmployeeEntity> entities);
 }
